7265714e2ccb9b543a8a40d29c9057a88e21c5a9
stslex <ilya977.077@gmail.com>
2025-02-08 19:46:41 +0300

    add migrations for chat_id

--- migrations/2025-02-08-164339_users/down.sql
+++ migrations/2025-02-08-164339_users/down.sql
@@ -9,6 +9,8 @@ DROP INDEX IF EXISTS users_is_accepted_index;
 
 DROP INDEX IF EXISTS users_is_active_index;
 
+DROP INDEX IF EXISTS users_chat_id_index;
+
 DROP TABLE IF EXISTS users;
 
 DROP EXTENSION IF EXISTS "uuid-ossp";
